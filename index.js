825f8a1d6f23256ac62e3cd6a45b9f08c6784b3c
gxp934 <gxp934@miami.edu>
2025-11-20 16:27:33 +0000

    my commit

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/helloagain', (req, res) => {
+  res.send('<h1>Hello again!</h1>')
+})
+
+app.get('/another', (req, res) => {
+  res.send('another route')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
